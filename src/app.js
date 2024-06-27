2707cae363fef21ee1869dbe3bd06cc7fa786107
becka777 <157316467+becka777@users.noreply.github.com>
2024-06-27 19:17:36 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extensiones = [".com", ".net", ".us"];
+
+function dominios() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let y = 0; y < noun.length; y++) {
+        for (let w = 0; w < extensiones.length; w++) {
+          console.log(pronoun[i] + adj[x] + noun[y] + extensiones[w]);
+        }
+      }
+    }
+  }
+}
+
+dominios();
